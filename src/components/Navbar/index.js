313e7a2b38b86161aea050dfb646d9379642dd06
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-05-20 16:29:50 -0400

    added fontawesome icons to navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,36 +8,43 @@ function Navbar() {
       <ul className='nav navbar-nav'>
         <li>
           <a href='#' className='settings'>
+            <i class='fas fa-wrench' />
             SETTINGS
           </a>
         </li>
         <li>
           <a href='#' className='gists'>
+            <i class='fas fa-code' />
             GISTS
           </a>
         </li>
         <li>
           <a href='#' className='stackOverflow'>
+            <i class='fab fa-stack-overflow' />
             STACK OVERFLOW
           </a>
         </li>
         <li>
           <a href='#' className='trending'>
+            <i class='fas fa-chart-line' />
             TRENDING
           </a>
         </li>
         <li>
           <a href='#' className='scratchPad'>
+            <i class='fas fa-pencil-alt' />
             SCRATCH PAD
           </a>
         </li>
         <li>
           <a href='#' className='playground'>
+            <i class='far fa-lightbulb' />
             PLAYGROUND
           </a>
         </li>
         <li>
           <a href='#' className='help'>
+            <i class='fas fa-question' />
             HELP/INFO
           </a>
         </li>
