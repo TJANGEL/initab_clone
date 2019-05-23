6808711d4d3da230479a47e896dff7e42e6a90c7
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-05-23 19:32:35 -0400

    added moment.js for time and date elements

--- js/main.js
+++ js/main.js
@@ -1,27 +1,50 @@
-// CLOCK //
 window.onload = function() {
   DisplayCurrentTime();
+  DisplayCurrentDate();
 };
+
+// CLOCK //
+var timeEl = document.getElementById('timeEl');
+
 function DisplayCurrentTime() {
-  var date = new Date();
-  var hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
-  var minutes =
-    date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
-  var suffix = 'AM';
-  if (hours >= 12) {
-    suffix = 'PM';
-    hours = hours - 12;
-  }
-  if (hours == 0) {
-    hours = 12;
-  }
+  let now = moment().format('LT');
 
-  time = hours + ':' + minutes + ' ' + suffix;
-  var timeEl = document.getElementById('timeEl');
-  timeEl.innerHTML = time;
-
-  setTimeout(DisplayCurrentTime, 1000);
+  timeEl.textContent = now;
 }
+
+setInterval(DisplayCurrentTime, 1000);
+
+// DATE //
+var dateEl = document.getElementById('dateEl');
+
+function DisplayCurrentDate() {
+  let now = moment().format('ll');
+
+  dateEl.textContent = now;
+}
+
+setInterval(DisplayCurrentDate, 1000);
+
+// function DisplayCurrentTime() {
+//   var date = new Date();
+//   var hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+//   var minutes =
+//     date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+//   var suffix = 'AM';
+//   if (hours >= 12) {
+//     suffix = 'PM';
+//     hours = hours - 12;
+//   }
+//   if (hours == 0) {
+//     hours = 12;
+//   }
+
+//   time = hours + ':' + minutes + ' ' + suffix;
+//   var timeEl = document.getElementById('timeEl');
+//   timeEl.innerHTML = time;
+
+//   setTimeout(DisplayCurrentTime, 1000);
+// }
 
 // On click remove text from screen //
 let gotIt = document.getElementById('got-it');
